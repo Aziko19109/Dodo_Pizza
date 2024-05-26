0f3a44460d618a41fc11dec0685a256933ddee07
Aziko <isaqov.19109.aziko@gmail.com>
2024-05-26 17:29:51 +0500

    firebase+

--- app/src/main/java/com/ziko/dodopizza/ui/SnacksFragment.kt
+++ app/src/main/java/com/ziko/dodopizza/ui/SnacksFragment.kt
@@ -6,8 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import com.ziko.dodopizza.R
 import com.ziko.dodopizza.adapter.NewsAdapter
+import com.ziko.dodopizza.databinding.FragmentPizzaBinding
 import com.ziko.dodopizza.databinding.FragmentSnacksBinding
 import com.ziko.dodopizza.model.Pizza
 
@@ -15,23 +22,73 @@ import com.ziko.dodopizza.model.Pizza
 class SnacksFragment : Fragment() {
 
     private lateinit var binding: FragmentSnacksBinding
-
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var reference: DatabaseReference
     private lateinit var newsAdapter: NewsAdapter
     private lateinit var list: ArrayList<Pizza>
+    private var lastId: Int = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
        binding = FragmentSnacksBinding.inflate(layoutInflater)
 
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        list = ArrayList()
+        takeLastId()
         newsAdapter = NewsAdapter(list)
         binding.rv.adapter = newsAdapter
-
         binding.backFromPizzaFragment.setOnClickListener(){
             findNavController().popBackStack()
         }
 
         return binding.root
+    }
+    fun takeLastId() {
+
+        reference = firebaseDatabase.getReference("snacks")
+
+        reference.orderByKey().limitToLast(1)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val lastKey = snapshot.children.last().key
+                        lastKey?.let {
+                            lastId = lastKey.toInt()
+                            loadData()
+
+                        }
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Обработка ошибок загрузки
+                }
+            })
+    }
+
+    fun loadData() {
+
+        for (i in 1..lastId) {
+            reference = firebaseDatabase.getReference("snacks/$i")
+            reference.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    val value = dataSnapshot.getValue<Pizza>()
+                    value?.let {
+                        if (!list.contains(it)) {
+                            list.add(it)
+                            newsAdapter.notifyDataSetChanged()
+                        }
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Обработка ошибок загрузки
+                }
+            })
+        }
     }
 
 
